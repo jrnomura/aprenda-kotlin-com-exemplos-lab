8b4a44b00f5f36eed9a17773218d57655f70f8f2
jrnomura <jrnomura6370@gmail.com>
2022-12-30 10:40:29 -0300

    Melhorando código do Desafio de Projeto

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,60 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel{BASICO, INTERMEDIARIO, AVANCADO}
 
-class Usuario
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class Usuario(val nome: String, val email: String)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class ConteudoEducacional(val nome: String, val tempo: Int, val nivel: Nivel)
 
-    val inscritos = mutableListOf<Usuario>()
-    
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
-    }
+data class Formacoes(val nome: String, val curso: List<ConteudoEducacional>){
+
+ val matricula = mutableListOf<Usuario>()
+
+ fun addUsuario(usuario: Usuario){
+   matricula.add(usuario)
 }
 
-fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+ fun getAddUsuario(){
+  for(usuario in matricula){
+    println("Nome: ${usuario.nome} - Email: ${usuario.email}")
+      }
+   }
+
+ fun imprimirConteudoEducacional(){
+  for(cursos in curso){
+    println("Nome: ${cursos.nome} - Tempo: ${cursos.tempo} - Nivel: cursos.nivel")
+   }
+  }
+}
+
+
+  fun main(){
+ val moduloJava = ConteudoEducacional("Linguagem Java", 150, Nivel.BASICO)
+
+ val moduloKotlin = ConteudoEducacional("Linguagem Kotlin", 120, Nivel.BASICO)
+
+ val listaCursos = mutableListOf<ConteudoEducacional>()
+ listaCursos.add(moduloJava)
+ listaCursos.add(moduloKotlin)
+
+ val jorge = Usuario("Jorge", "jorge@gmail.com")
+ val maria = Usuario("Maria", "maria@gmail.com")
+
+ val treinamentoAndroid = Formacoes("Curso Android", listaCursos)
+ treinamentoAndroid.addUsuario(jorge)
+ treinamentoAndroid.addUsuario(maria)
+
+ println("$treinamentoAndroid")
+
+ println(" ")
+ 
+ println("Cursos")
+ println(listaCursos)
+ 
+ println(" ")
+ 
+ println("Alunos Matriculados: ")
+ println(treinamentoAndroid.getAddUsuario())
+
 }
